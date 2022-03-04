626cc0395d6cfbfbf0a46c9e078150b4ab7ab2a2
Andres <galoan2001@uma.es>
2022-03-04 11:55:15 +0100

    Added negative entry checker

--- src/main/java/fibonacci/Fibonnaci.java
+++ src/main/java/fibonacci/Fibonnaci.java
@@ -1,7 +1,11 @@
 package fibonacci;
 
+import java.util.InputMismatchException;
+
 public class Fibonnaci {
-    public static int compute(int number){
+    public int compute(int number){
+        if (number < 0)
+            throw new InputMismatchException("ERROR: Number: "+number+ " is negative");
         return number;
     }
 }
