2f2b561c0db2424df734b9e9bef49abf5abd2287
Andres <galoan2001@uma.es>
2022-03-04 14:22:58 +0100

    Added Base case 0 and comments

--- src/main/java/fibonacci/Fibonnaci.java
+++ src/main/java/fibonacci/Fibonnaci.java
@@ -12,10 +12,11 @@ public class Fibonnaci {
         if(number > 46) //Integer Max Value overflow
             throw new RuntimeException("ERROR: The maximum value is 47 due to Integer.MAXVALUE overflow. Your entry was:"+ number);
         int values[] = new int[number+2];
-        values[1] = 1;
+        values[0] = 0; //Base case
+        values[1] = 1; //Base case
 
         for(int i = 2; i<= number; i++){
-         values[i] = values[i-1] + values[i-2];
+         values[i] = values[i-1] + values[i-2]; //Insert the new calculated value to use it later
         }
         return values[number];
     }
